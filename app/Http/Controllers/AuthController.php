e55a3b5cc28c8eef934a182741b78cc6ba772f76
sorraine <kalifasroberts@gmail.com>
2024-04-18 19:00:28 -0400

    Edited Auth Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,43 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-class AuthController extends Controller
-{
-    //
-}
-<?php
-
-// app/Http/Controllers/AuthController.php
-
-namespace App\Http\Controllers;
-
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Validator; 
+
+
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
+        $validator = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6'
         ]);
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $user =new User();
 
         $token = $user->createToken('API Token')->accessToken;
 
@@ -47,27 +30,28 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|string|email',
-            'password' => 'required|string'
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        if (Auth::attempt($request->only('email', 'password'))) {
-            $user = Auth::user();
-            $token = $user->createToken('API Token')->accessToken;
-            return response()->json(['token' => $token], 200);
-        }
+        {
+            $credentials = request(['email', 'password']);
+            if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+            }
+            return response()->json([
+            'message' => 'Login Successful!',
+            'access_token' => $token,
+            ]);
+            }
 
         return response()->json(['message' => 'Unauthorized'], 401);
     }
 
-    public function logout(Request $request)
-    {
-        $request->user()->token()->revoke();
-        return response()->json(['message' => 'Successfully logged out'], 200);
-    }
+  /**
+ * Log the user out (Invalidate the token).
+ *
+ * @return \Illuminate\Http\JsonResponse
+ */
+     public function logout()
+        {
+     auth()->logout();
+        return response()->json(['message' => 'Successfully logged out']);
+     }   
 }
