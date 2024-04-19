cffd6e2c9f6b903a35372418aabeb259bc12d7b4
Faby-Baby <fabianbertrand94@gmail.com>
2024-04-18 21:22:14 -0400

    Edit route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import AboutView from "../Pages/AboutView.vue";
 import HomeView from "../Pages/HomeView.vue";
 import RegisterView from "../Pages/RegisterView.vue";
+import LoginView from "../Pages/LoginView.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
