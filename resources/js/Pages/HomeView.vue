3c8259e1103192b458fbf88ea9a865d07c9d6c19
kellyandra <ms.kchristian86@gmail.com>
2024-04-19 20:26:02 -0400

    updated files

--- resources/js/Pages/HomeView.vue
+++ resources/js/Pages/HomeView.vue
@@ -4,17 +4,7 @@ import McLaren from '../../images/McLaren.jpg';
 
 <template>
   <div class="container py-5">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
-      <div class="container">
-        <router-link class="navbar-brand" to="/">United Auto Sales</router-link>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav ms-auto">
-            <router-link to="/register" class="nav-link">Register</router-link>
-            <router-link to="/login" class="nav-link">Login</router-link>
-          </div>
-        </div>
-      </div>
-    </nav>
+   
     
     <div class="row align-items-center">
       <div class="col-md-6 text-start">
