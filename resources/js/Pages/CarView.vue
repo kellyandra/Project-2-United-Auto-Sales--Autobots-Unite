12c24a4dee9113f055e6ba0e1b11397ebc16361e
Faby-Baby <fabianbertrand94@gmail.com>
2024-04-19 20:12:30 -0400

    Edit car view

--- resources/js/Pages/CarView.vue
+++ resources/js/Pages/CarView.vue
@@ -6,37 +6,37 @@
 </script>
 
 <template>
- <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
+  <div class="card mb-3" style="max-width: 540px;">
+    <div class="row g-0">
     <div class="col-md-6">
-      <img src="{{ url('storage/' . $property->propertyphoto) }}" class="img-fluid rounded-start" alt="...">
+      <img src="{{ }}" class="img-fluid rounded-start" alt="...">
     </div>
     <div class="col-md-6">
       <div class="card-body">
-        <h5 class="card-title">{{ $property->title }}</h5> 
+        <h5 class="card-title">{{ }}</h5> 
 
         
-        <p class="card-text badge rounded-pill bg-primary"> ${{$property->price}} </p>
+        <p class="card-text badge rounded-pill bg-primary"> ${{}} </p>
 
 
-        <p class="card-text badge rounded-pill bg-warning"> {{$property->type}} </p>
-        <p class="card-text"> {{$property->description}} </p>
+        <p class="card-text badge rounded-pill bg-warning"> {{}} </p>
+        <p class="card-text"> {{}} </p>
 
         <p class="card-text"> </p>
                     
                   
 
         <p class="card-text">
-                    {{$property->numBathrooms}} Bathrooms </p>
+                    {{}} Bathrooms </p>
 
         <p class="card-text"> 
-                         {{$property->location}} </p>
+                         {{}} </p>
         <button type="submit" class=" text-white btn " style="background:#2cab3b;" disabled>Email Owner</button>
         
-</div>
+      </div>
     </div>
   </div>
-</div>
+  </div>
 
 
 </template>
